a2ff98279c7c138ad4e4f59b54b7a2381e80852a
Rigoz <zhaky7@gmail.com>
2014-02-25 01:45:53 +0100

    Added DeepCopy to constructor/op=

--- Record.h
+++ Record.h
@@ -17,18 +17,24 @@ private:
 	Record *m_next;
 	
 public:
-	Record(const Item &item = 0);
+	Record(const Item &item = 0, Record *prev = NULL, Record *next = NULL);
 	Record(const Record &record);
 	~Record();
 	
+	// OPs
 	bool operator==(const Record &record) const;
 	bool operator!=(const Record &record) const;
 	bool operator>(const Record &record) const;
 	bool operator<(const Record &record) const;
 	bool operator>=(const Record &record) const;
 	bool operator<=(const Record &record) const;
+	Record<Item>& operator=(const Record &record);
 	
 	friend std::ostream& operator<< <>(std::ostream &os, const Record<Item> &record);
+	
+	// Helpers
+	static void DeepCopy(Record *r1, Record *r2);
+	Record<Item>* Next(Record *r2);
 };
 
 #include "Record.cpp"
