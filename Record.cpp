b4d5aebb5d4926ea7a0668b200cdb00d4a1ca457
Rigoz <zhaky7@gmail.com>
2014-02-27 13:27:15 +0100

    Update

--- Record.cpp
+++ Record.cpp
@@ -1,8 +1,8 @@
 template <class Item>
-Record<Item>::Record(const Item &item,  Record *prev, Record *next) : m_info(item), m_prev(prev), m_next(next) {}
+Container<Item>::Record::Record(const Item &item,  Container<Item>::Record *prev, Record *next) : m_info(item), m_prev(prev), m_next(next) {}
 
 template <class Item>
-Record<Item>::Record(const Record &record)
+Container<Item>::Record::Record(const Container<Item>::Record &record)
 {
 	if (*this != record)
 	{
@@ -13,49 +13,49 @@ Record<Item>::Record(const Record &record)
 }
 
 template <class Item>
-Record<Item>::~Record()
+Container<Item>::Record::~Record()
 {
 	delete m_next;
 }
 
 template <class Item>
-bool Record<Item>::operator==(const Record &record) const
+bool Container<Item>::Record::operator==(const Record &record) const
 {
 	return m_info == record.m_info;
 }
 
 template <class Item>
-bool Record<Item>::operator!=(const Record &record) const
+bool Container<Item>::Record::operator!=(const Record &record) const
 {
 	return m_info != record.m_info;
 }
 
 template <class Item>
-bool Record<Item>::operator>(const Record &record) const
+bool Container<Item>::Record::operator>(const Record &record) const
 {
 	return m_info > record.m_info;
 }
 
 template <class Item>
-bool Record<Item>::operator<(const Record &record) const
+bool Container<Item>::Record::operator<(const Record &record) const
 {
 	return m_info < record.m_info;
 }
 
 template <class Item>
-bool Record<Item>::operator>=(const Record &record) const
+bool Container<Item>::Record::operator>=(const Record &record) const
 {
 	return m_info >= record.m_info;
 }
 
 template <class Item>
-bool Record<Item>::operator<=(const Record &record) const
+bool Container<Item>::Record::operator<=(const Record &record) const
 {
 	return m_info <= record.m_info;
 }
 
 template <class Item>
-Record<Item>& Record<Item>::operator=(const Record &record)
+typename Container<Item>::Record& Container<Item>::Record::operator=(const Record &record)
 {
 	if (*this != record)
 	{
@@ -66,50 +66,17 @@ Record<Item>& Record<Item>::operator=(const Record &record)
 }
 
 template <class Item>
-std::ostream& operator<<(std::ostream &os,const Record<Item> &record)
-{
-	if (record.m_prev == NULL)
-		os << "START -> \n";
-	
-	os << &record << " = | " << record.m_prev << " ";
-	os << record.m_info << " ";
-	os << record.m_next << " | -> \n" ;
-	
-	if (record.m_next == NULL)
-		os << "END\n";
-	else
-		os << *(record.m_next);
-		
-	return os;
-}
-
-template <class Item>
-void Record<Item>::DeepCopy(Record *r1, Record *r2)
+void Container<Item>::Record::DeepCopy(Container<Item>::Record *r1, Container<Item>::Record *r2)
 {
 	if (r2 == NULL)
 		return;
 	
-	r1->m_next = new Record<Item>(r2->m_info, r1);
+	r1->m_next = new Container<Item>::Record(r2->m_info, r1);
 	DeepCopy(r1->m_next, r2->m_next);
 }
 
 template <class Item>
-Record<Item>* Record<Item>::Next(Record *r)
-{
-	m_next = r;
-	return r;
-}
-
-
-template <class Item>
-Record<Item>* Record<Item>::Prev(Record *r)
-{
-	m_prev = r;
-	return r;
-}
-
-template <class Item>
-Record<Item>* Record<Item>::Update(const Item &item, Record *prev, Record *next)
+void Container<Item>::Record::Update(const Item &item, Container<Item>::Record *prev, Container<Item>::Record *next)
 {
 	m_info = item;
 	
@@ -118,6 +85,10 @@ Record<Item>* Record<Item>::Update(const Item &item, Record *prev, Record *next)
 	
 	if (next != NULL)
 	    m_next = next;
-	
-	return this;
+}
+
+template <class Item>
+Item Container<Item>::Record::GetData()
+{
+	return m_info;
 }
