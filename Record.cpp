c72916d81afcb0d512f5fd53fcec65b9e5e05c4a
Rigoz <zhaky7@gmail.com>
2014-03-03 12:04:12 +0100

    Update Record.cpp

--- Record.cpp
+++ Record.cpp
@@ -1,5 +1,5 @@
 template <class Item>
-Container<Item>::Record::Record(const Item &item,  Container<Item>::Record *prev, Record *next) : m_info(item), m_prev(prev), m_next(next) {}
+Container<Item>::Record::Record(const Item &item,  Container<Item>::Record *prev, Container<Item>::Record *next) : m_info(item), m_prev(prev), m_next(next) {}
 
 template <class Item>
 Container<Item>::Record::Record(const Container<Item>::Record &record)
@@ -8,14 +8,14 @@ Container<Item>::Record::Record(const Container<Item>::Record &record)
 	{
 		m_prev = NULL;
 		m_info = record.m_info;
-		m_next = DeepCopy(this, record.m_next);
+		m_next = DeepCopy(this, record.m_next); // previous Record to attach, next Record to copy
 	}
 }
 
 template <class Item>
 Container<Item>::Record::~Record()
 {
-	delete m_next;
+	delete m_next; // cascade delete
 }
 
 template <class Item>
@@ -55,20 +55,21 @@ bool Container<Item>::Record::operator<=(const Record &record) const
 }
 
 template <class Item>
-typename Container<Item>::Record& Container<Item>::Record::operator=(const Record &record)
+typename Container<Item>::Record& Container<Item>::Record::operator=(Record record)
 {
-	if (*this != record)
-	{
-		m_info = record.m_info;
-		m_prev = NULL;
-		DeepCopy(this, record.m_next);
-	}
+	Swap(*this, record); // swap list pointers
+	
+	// we dont need to delete our old data since it now
+	// belongs to "record", which is a local copy and will
+	// be destroyed after the return statement
+	return *this;
 }
 
+// Creates a new Record copying it from "curr" and attaching it to the "prev"
 template <class Item>
 typename Container<Item>::Record* Container<Item>::Record::DeepCopy(Container<Item>::Record *prev , Container<Item>::Record *curr)
 {	
-	if (curr == NULL)
+	if (curr == NULL) // Reached the end of the list to copy
 	    return NULL;
 	
 	Record *t = new Record(curr->m_info);
@@ -77,18 +78,7 @@ typename Container<Item>::Record* Container<Item>::Record::DeepCopy(Container<It
 	return t;                                                   
 }
 
-template <class Item>
-void Container<Item>::Record::Update(const Item &item, Container<Item>::Record *prev, Container<Item>::Record *next)
-{
-	m_info = item;
-	
-	if (prev != NULL)
-	    m_prev = prev;
-	
-	if (next != NULL)
-	    m_next = next;
-}
-
+// Returns data contained in m_info
 template <class Item>
 Item Container<Item>::Record::GetData() const
 {
