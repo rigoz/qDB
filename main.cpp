47030f69f434c2238428ca620c95383be52b7097
Rigoz <zhaky7@gmail.com>
2014-03-03 12:04:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,42 @@ using namespace std;
 
 int main()
 {
-	Container<int> a(5);
-	a.Insert(7.5f);
-	a.Insert('c');
-	a.Insert(true);
-	a.Insert(99);
+	Container<int> a(1);
+	a.Insert(2);
+	a.Insert(3);
+	/*a.Insert(4);
+	a.Insert(5);
+	a.Insert(6);
+	a.Insert(7);
+	a.Insert(8);*/
 	
-	cout << a;
+	Container<int> b(1);
+	b.Insert(2);
+	b.Insert(3);
 	
-
-	a.~Container();
-	cout << a;
+	cout <<  a - b;
+	
+	//a.Remove(1);
+	//a.Remove(2);
+	//a.Remove(3);
+	cout << "fine\n";
+	
+	
+	/*
+	Container<int> h(a);
+	cout << h;
+	Container<int> c;
+	
+	c = a + b;
+	
+	cout << "A = " << a << "B = " << b << "C = " << c;
+	Container<int> d;
+	d = a - b;
+	cout  << "D = " << d;
+	
+	if (c.Find(20))
+		cout << "trovato\n";
+	else
+		cout << "non trovato\n";*/
+	
 }
