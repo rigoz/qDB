59d5a00543b6eb73d8bbcdaec2f20b38c74c1aa5
Rigoz <zhaky7@gmail.com>
2014-03-04 01:30:20 +0100

    Update SharedDefines.h

--- SharedDefines.h
+++ SharedDefines.h
@@ -1,8 +1,24 @@
 #ifndef SHAREDDEFINES_H
-#define SHAREDDEFINES_H
 
-#ifndef NULL
-	#define NULL 0
-#endif
+#	define SHAREDDEFINES_H
+
+#	include <cstdlib>
+
+#	ifndef NULL
+#		define NULL 0
+#	endif
+
+#	ifndef NDEBUG
+#   	define ASSERT_PTR(condition) \
+			if (!(condition)) \
+			{ \
+				std::cout << std::endl << "<-!->" << std::endl; \
+				std::cout << "Something went wrong! NULL pointer dereferenced on file \"" \
+				<< __FILE__ << "\", line " << __LINE__ << std::endl << "Program execution has been stopped." << std::endl \
+				<< "<-!->" << std::endl; \
+				std::exit(EXIT_FAILURE); \
+			}
+#	endif
+
 
 #endif
