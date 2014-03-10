25a4f30b14f9e0bd450637f888f65326c88814a5
Rigoz <zhaky7@gmail.com>
2014-03-10 11:42:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,19 @@
 #include <iostream>
+#include <string>
 #include "Container.h"
-
+#include "Match.h"
+#include "SharedDefines.h"
 using namespace std;
 
 int main()
 {
-	Container<int> a(1);
-	a.Insert(2);
-	a.Insert(3);
-	/*a.Insert(4);
-	a.Insert(5);
-	a.Insert(6);
-	a.Insert(7);
-	a.Insert(8);*/
+	string name("Ciao");
+	string surname("Miao");
+	Referee r;
+	r.name = "CIAO";
+	r.surname = "MIAO";
+	r.telephone = "0495590908";
+	Match m(r, 57, "Piazzola");
 	
-	Container<int> b(1);
-	b.Insert(2);
-	b.Insert(3);
-	
-	cout <<  a - b;
-	
-	//a.Remove(1);
-	//a.Remove(2);
-	//a.Remove(3);
-	cout << "fine\n";
-	
-	
-	/*
-	Container<int> h(a);
-	cout << h;
-	Container<int> c;
-	
-	c = a + b;
-	
-	cout << "A = " << a << "B = " << b << "C = " << c;
-	Container<int> d;
-	d = a - b;
-	cout  << "D = " << d;
-	
-	if (c.Find(20))
-		cout << "trovato\n";
-	else
-		cout << "non trovato\n";*/
-	
+	cout << m.GetId();
 }
