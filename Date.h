11dd8bb57adbdf110d1ffa5d73bff1746c4ce9a6
Rigoz <zhaky7@gmail.com>
2014-03-11 15:50:47 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -32,56 +32,33 @@ private:
 	    return ((year-firstLeapyear) % leapRecurrence) == 0;
 	    
 	}
+	
+	static uint16 GetLeapYears(uint16 year)
+	{
+	    return (year < firstLeapyear) ? 0 : ((year-firstLeapyear) / leapRecurrence);
+	}
 
 public:
 	Date() : m_days(0) {}
     
-	Date(uint32 days) : m_days(days) {}
+	Date(int days)
+	{
+	    m_days = days < 0 ? 0 : days;
+	}
 	
 	Date(uint16 day, uint16 month, uint16 year)
 	{
-	    uint16 daysForYear = 0;
-	    uint16 leapYears = (year < firstLeapyear) ? 0 : ((year-firstLeapyear) / leapRecurrence);
-	    if (year > baseYear)
-		daysForYear = (year - baseYear) * 365 + leapYears;
+	    uint16 daysForYear = (year > baseYear) ? (year - baseYear) * 365 + GetLeapYears(year) : 0;
 	    
 	    m_days = day-1 + GetDaysUntilMonth(month) + daysForYear + (IsLeapYear(year) && month > 2 ? 1 : 0);
 	}
 	
 	Date(const Date &date) : m_days(date.m_days) {}
 	
-	/*uint16 GetDay() const
-	{
-	    uint16 days = m_days % 365;
-	    for (int i=0; i<12; i++)
-		if (days > monthsLength[i])
-		    days -= monthsLength[i];
-	    
-	    days -= 
-	}
-	
-	uint16 GetMonth() const
-	{
-	    uint16 days = m_days % 365;
-	    for (int i=0; i<12; i++)
-		if (days < monthsLength[i])
-		    return i+1;
-		else
-		    days -= monthsLength[i];
-	    
-	    return days;
-	}
-	
-	uint16 GetYear() const
-	{
-	    return m_days / 365 + baseYear;
-	}*/
-	
 	std::string ToString() const
 	{
 	    uint16 year = m_days / 365 + baseYear;
-	    uint16 leapYears = (year < firstLeapyear) ? 0 : ((year-firstLeapyear) / leapRecurrence);
-	    uint16 days = m_days % 365 - leapYears + 1;
+	    uint16 days = m_days % 365 - GetLeapYears(year) + 1;
 	    uint16 month = 1;
 	    for (int i=0; i<12; i++)
 		if (days > monthsLength[i])
@@ -99,4 +76,19 @@ public:
 	{
 	    return os << date.ToString();
 	}
+	
+	Date operator+(int n)
+	{
+	    Date t(m_days + n);
+	    return t;
+	}
+	
+	Date operator-(int n)
+	{
+	    Date t(m_days - n);
+	    return t;
+	}
 };
+
+
+#endif
