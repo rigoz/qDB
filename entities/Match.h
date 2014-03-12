d44f492989deea0852943cbd419f943e0c8ac8ab
Rigoz <zhaky7@gmail.com>
2014-03-12 12:00:55 +0100

    Update Match.h

--- entities/Match.h
+++ entities/Match.h
@@ -3,30 +3,23 @@
 
 #include <string>
 #include <ctime>
+#include <iostream>
 #include "SharedDefines.h"
 #include "PhoneNumber.h"
 #include "Date.h"
 
 struct Result
 {
-	uint16 home;
-	uint16 guest;
+	int16 home;
+	int16 guest;
 };
-
-struct Referee
-{
-	std::string surname;
-	std::string name;
-	PhoneNumber telephone;
-};
-
 
 class Match
 {
 private:
 	uint32 m_id;
 	Result m_result;
-	Referee m_referee;
+	std::string m_referee;
 	Date m_date;
 	std::string m_location;
 	bool m_closed;
@@ -35,27 +28,32 @@ private:
 	uint32 GetNextId() const;
 	
 public:
-	Match(Referee ref, Date date, std::string location);
+	Match(std::string ref, Date date, std::string location);
 	Match(const Match &m);
-	virtual ~Match();
+	//virtual ~Match();
 	
 	// <--------- Getters ------------
 	uint32 GetId() const;
 	Result GetResult() const;
-	Referee GetReferee() const;
+	std::string GetReferee() const;
 	Date GetDate() const;
 	std::string GetLocation() const;
 	bool IsClosed() const;
 	// ------------------------------>
 	
 	// <--------- Setters -----------
-	virtual void SetResult(uint16 homeScore, uint16 guestScore);
-	void SetReferee(Referee newReferee);
+	virtual void SetResult(int16 homeScore, int16 guestScore);
+	void SetReferee(std::string newReferee);
 	void Postpone(Date newDate);
 	void SetLocation(std::string location);
 	virtual void Close();
 	// ------------------------------>
 	
+	// <-------- Operators -----------
+	bool operator==(const Match &mtc) const;
+	bool operator!=(const Match &mtc) const;
+	friend std::ostream& operator<<(std::ostream &os, const Match &mtc);
+	// ------------------------------>
 };
 
 #endif
